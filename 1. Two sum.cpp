c7608a049b6552052294a4be335b9940ebcf1f4e
Abhinav <abhinavtiwari2705@gmail.com>
2023-05-04 22:31:13 +0530

    Two sum

---
+++ 1. Two sum.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> ans;
+        unordered_map<int, int> mep;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (mep.find(target - nums[i]) != mep.end())
+            {
+                ans.push_back(mep[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            mep[nums[i]] = i;
+        }
+
+        return ans;
+    }
+};
