f1d4779d49039a401311c27b661a996e9b54cd50
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-05-20 00:45:26 +0530

    Create 704. Binary Search.cpp

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int r=nums.size()-1,l=0;
+        
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+            mid=r+(l-r)/2;
+        }
+        return -1;
+    }
+};
