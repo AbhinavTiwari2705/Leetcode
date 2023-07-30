1bbe96aff4fe5ed57828483151056e200ece409f
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-07-30 21:49:24 +0530

    Create 2798. Number of Employees Who Met the Target.cpp

---
+++ 2798. Number of Employees Who Met the Target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count=0;
+        for(auto i:hours){
+            if(i>=target){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
