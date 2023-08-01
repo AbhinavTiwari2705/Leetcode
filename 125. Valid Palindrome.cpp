e514e40ce71ed3b5149d3af4a950a97b93e4d067
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-08-02 00:28:52 +0530

    Create 125. Valid Palindrome.cpp

---
+++ 125. Valid Palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int st = 0;
+        int e = s.size() - 1;
+
+        while(st<=e) {
+            if (!isalnum(s[st])) st++;
+            else if (!isalnum(s[e])) e--;
+
+            else if (toupper(s[st]) != toupper(s[e])) return false;
+
+            else{
+                st++;
+                e--;
+
+            }
+
+
+        }
+
+        return true;
+    }
+};
