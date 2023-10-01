70d9b6ec7bafc1a41fb17600666c40c1b619716e
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-10-01 22:21:25 +0530

    Create 1004. Max Consecutive Ones III.cpp

---
+++ 1004. Max Consecutive Ones III.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0,j=0,n=nums.size(),zerocnt=0,ans=INT_MIN;
+        while(j<n){
+            if(nums[j]==0){
+                zerocnt++;
+            }
+            while(zerocnt>k){
+                if(nums[i]==0){
+                    zerocnt--;
+                }
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
