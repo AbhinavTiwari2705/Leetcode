ad158e51869efad91737dd7807efd518c8b5abff
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-10-22 23:28:58 +0530

    Create 71. Simplify Path.cpp

---
+++ 71. Simplify Path.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+
+        for (int i = 0; i < path.size(); i++) {
+            if (path[i] == '/') {
+                continue;
+            }
+
+            string temp;
+            while (i < path.size() && path[i] != '/') {
+                temp += path[i];
+                i++; 
+            }
+
+            if (temp == ".") {
+                continue;
+            } else if (temp == "..") {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            } else {
+                st.push(temp);
+            }
+        }
+
+        string res;
+        while (!st.empty()) {
+            res = "/" + st.top() + res; 
+            st.pop(); 
+        }
+
+        if (res.empty()) {
+            return "/";
+        }
+        return res;
+    }
+};
