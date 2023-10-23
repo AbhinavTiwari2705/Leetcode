84ecbfacf7cd8d887842fef60d080c5cd4f02a71
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-10-23 23:37:46 +0530

    Create 1021. Remove Outermost Parentheses.cpp

---
+++ 1021. Remove Outermost Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        stack<char>st;
+        for(auto i:s){
+            if(i=='('){
+                if(!st.empty()) ans+=i;
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(!st.empty()){
+                    ans+=i;
+                }
+            }
+
+            
+        }
+        return ans;
+    }
+};
