f59f3c2049a852906f384965e0f3db79aa7e0106
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-11-28 19:23:42 +0530

    Update 1004. Max Consecutive Ones III.cpp

--- 1004. Max Consecutive Ones III.cpp
+++ 1004. Max Consecutive Ones III.cpp
@@ -5,6 +5,7 @@ public:
         int i=0,j=0,n=nums.size(),zerocnt=0,ans=INT_MIN;
         while(j<n){
             if(nums[j]==0){
+                
                 zerocnt++;
                 
             }
