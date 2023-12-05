9650f3b2d6bd37eba66705b88f213113c707ad11
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-12-05 23:10:49 +0530

    Create 1209. Remove All Adjacent Duplicates in String II.cpp

---
+++ 1209. Remove All Adjacent Duplicates in String II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char, int>> st;
+        string ans;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (st.empty() || st.top().first != s[i]) {
+                st.push({s[i], 1});
+            } else {
+                auto prev = st.top();
+                st.pop();
+                st.push({s[i], prev.second + 1});
+            }
+
+           
+            if (!st.empty() && st.top().second == k) {
+                st.pop();
+            }
+        }
+
+        
+        while (!st.empty()) {
+            auto curr = st.top();
+            st.pop();
+            while (curr.second--) {
+                ans += curr.first;
+            }
+        }
+
+
+        reverse(ans.begin(), ans.end());
+
+        return ans; 
+    }
+};
