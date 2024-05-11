7b3bc8e04ddebda25d42b9ffa8c9929a5eaeadb5
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-05-11 23:06:00 +0530

    Create 165. Compare Version Numbers.cpp

---
+++ 165. Compare Version Numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int i=0,j=0,v1len=v1.size(),v2len=v2.size(),n1,n2;
+
+        while(i<v1len||j<v2len){
+            n1=0;
+            n2=0;
+            while(i<v1len&&v1[i]!='.'){
+                n1=n1*10+(v1[i]-'0');
+                i++;
+            }
+            
+            while(j<v2len&&v2[j]!='.'){
+                n2=n2*10+(v2[j]-'0');
+                j++;
+            }
+            if(n1>n2) return 1;
+            if(n2>n1) return -1;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
