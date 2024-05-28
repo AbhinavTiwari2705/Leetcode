74b2dd5dd236e33239c9d9325f7413c9687fd621
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-05-28 21:36:19 +0530

    Create 429. N-ary Tree Level Order Traversal.cpp

---
+++ 429. N-ary Tree Level Order Traversal.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> ans;
+        if(root == nullptr) return ans;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i = 0; i < size; i++){
+                Node* curr = q.front();
+                q.pop();
+                level.push_back(curr->val);
+                for(Node* child : curr->children){
+                    q.push(child);
+                }
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
