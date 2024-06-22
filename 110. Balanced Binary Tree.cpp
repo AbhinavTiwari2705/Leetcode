b7672f693d42bcab3719602f798e864ba603123f
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-06-22 22:04:52 +0530

    Update 110. Balanced Binary Tree.cpp

--- 110. Balanced Binary Tree.cpp
+++ 110. Balanced Binary Tree.cpp
@@ -17,6 +17,7 @@ public:
         int lh=height(root->left);
         if(lh==-1) return -1;
         
+        
         int rh=height(root->right);
         if(rh==-1) return -1;
         if(abs(lh-rh)>1) return -1;
