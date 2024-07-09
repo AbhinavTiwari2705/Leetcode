3c4cbb1d9b7c5cd5decfbbb933f0e82c51e4569f
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2024-07-09 23:28:05 +0530

    Update 74. Search a 2D Matrix.cpp with binary search

--- 74. Search a 2D Matrix.cpp
+++ 74. Search a 2D Matrix.cpp
@@ -12,3 +12,29 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int n= arr.size();
+        int m = arr[0].size();
+
+        int low =0;
+        int high=n*m-1;
+
+        
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col= mid%m;
+
+            if(arr[row][col]==target) return true;
+
+            if(arr[row][col]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return 0;
+    }
+};
